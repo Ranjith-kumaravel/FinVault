5e96f8b56e8e93728e36d41272aa97b666a7cda7
Ranjith-kumaravel <ranjithk160106@gmail.com>
2026-06-24 14:28:40 +0530

    Add files via upload

---
+++ frontend/src/services/api.js
@@ -0,0 +1,98 @@
+import axios from "axios";
+
+//const API = "[localhost](http://localhost:5001)";
+
+const API = "http://localhost:5001";
+
+export const signup = (data) => {
+  return axios.post(`${API}/signup`, data);
+};
+
+export const login = (accountNumber, pin) => {
+  return axios.post(`${API}/login`, {
+    accountNumber,
+    pin,
+  });
+};
+
+export const getBalance = (accountNumber) => {
+  return axios.get(
+    `${API}/balance/${accountNumber}`,
+    {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem(
+          "token"
+        )}`,
+      },
+    }
+  );
+};
+
+export const deposit = (
+  accountNumber,
+  amount
+) => {
+  return axios.post(
+    `${API}/deposit`,
+    {
+      accountNumber,
+      amount,
+    },
+    {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem(
+          "token"
+        )}`,
+      },
+    }
+  );
+};
+
+export const withdraw = (
+  accountNumber,
+  amount
+) => {
+  return axios.post(
+    `${API}/withdraw`,
+    {
+      accountNumber,
+      amount,
+    },
+    {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem(
+          "token"
+        )}`,
+      },
+    }
+  );
+};
+
+export const getAccount = (
+  accountNumber
+) => {
+  return axios.get(
+    `${API}/account/${accountNumber}`,
+    {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem(
+          "token"
+        )}`,
+      },
+    }
+  );
+};
+
+export const transferMoney = (data) => {
+  return axios.post(
+    `${API}/transfer`,
+    data,
+    {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem(
+          "token"
+        )}`,
+      },
+    }
+  );
+};
